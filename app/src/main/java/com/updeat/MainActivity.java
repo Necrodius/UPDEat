5585d420bc2911d7973c34eaec6007134e842f42
nunezrrr <rayrivera322@gmail.com>
2023-02-28 21:19:22 +0800

    main_and_loginpage
    +added the main activity page and login page
    +added the login button functionality in the main activity page

---
+++ app/src/main/java/com/updeat/MainActivity.java
@@ -0,0 +1,32 @@
+package com.updeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogIn= (Button) findViewById(R.id.btnLogIn);
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogInMain();
+            }
+        });
+    }
+    public void openLogInMain() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+    }
+
+}
