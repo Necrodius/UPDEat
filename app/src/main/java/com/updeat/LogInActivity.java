c039dcea0798714277db721e794150054b8f1a6c
Javi Nicolas <jnicolas@up.edu.ph>
2023-03-07 20:12:40 +0800

    added UC 3.0 S0

--- app/src/main/java/com/updeat/LogInActivity.java
+++ app/src/main/java/com/updeat/LogInActivity.java
@@ -2,13 +2,30 @@ package com.updeat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+    private Button btnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnConfirm = (Button) findViewById(R.id.btnConfirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openViewDashboard();
+            }
+        });
     }
+    public void openViewDashboard() {
+        Intent intentDash = new Intent(LogInActivity.this, DashboardActivity.class);
+        startActivity(intentDash);
+    }
+
 }
--- app/src/main/java/com/updeat/MainActivity.java
+++ app/src/main/java/com/updeat/MainActivity.java
@@ -5,12 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnLogIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +27,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LogInActivity.class);
         startActivity(intent);
     }
+
 
 }
